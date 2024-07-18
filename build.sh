e13b6120c26ba07fbf15b1f3ed0d800e3481a881
Jonathan Harden <jonathan.harden@digital.cabinet-office.gov.uk>
2024-07-18 18:29:38 +0100

    PP-12937: Fix gomego version lookup

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -ex
 go mod download
 
 GINKGO_VERSION=$(grep "onsi\/ginkgo" go.mod | awk '{ print $2; }')
-GOMEGA_VERSION=$(grep "onsi\/ginkgo" go.mod | awk '{ print $2; }')
+GOMEGA_VERSION=$(grep "onsi\/gomega" go.mod | awk '{ print $2; }')
 
 go get "github.com/onsi/ginkgo/ginkgo@${GINKGO_VERSION}"
 go get "github.com/onsi/gomega@${GOMEGA_VERSION}"
